7b29a719e98787654dc4c5c95483512e7763f267
Tonny <tonnyan30@gmail.com>
2026-05-02 17:14:20 -0700

    Checkboxes working

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,20 @@ function App() {
     setTodoList(previous => [newTodo, ...previous]);
   }
 
+  function completeTodo (id) {  
+    const updatedTodoList = todoList.map((todo) => 
+    {
+      if (todo.id === id) return { ...todo, isCompleted: true}
+      else return todo;
+    })
+    setTodoList(updatedTodoList);
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}/>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,16 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({todoList}) {
-    
+function TodoList({todoList, onCompleteTodo}) {
+  
     return (
       <ul>
-        {todoList.length === 0 ?  <p> Add todo above to get started </p>: <></>} 
+        {todoList.length === 0 ? <p> Add todo above to get started </p>: null} 
         {todoList.map((todo) => {
           return(
             <TodoListItem
               key={todo.id}
               todo={todo}
+              onCompleteTodo={onCompleteTodo}
             />
           );
         })}
