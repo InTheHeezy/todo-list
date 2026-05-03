2c5e1a647ab8f11424a4687466205a32355de30c
Tonny <tonnyan30@gmail.com>
2026-05-02 17:29:51 -0700

    Spacing

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,23 @@
 import TodoListItem from "./TodoListItem";
 
 function TodoList({todoList, onCompleteTodo}) {
+
   
-    return (
-      <ul>
-        {todoList.length === 0 ? <p> Add todo above to get started </p>: null} 
-        {todoList.map((todo) => {
-          return(
-            <TodoListItem
-              key={todo.id}
-              todo={todo}
-              onCompleteTodo={onCompleteTodo}
-            />
-          );
-        })}
-      </ul>
-    );
+
+  return (
+    <ul>
+      {todoList.length === 0 ? <p> Add todo above to get started </p>: null} 
+      {todoList.map((todo) => {
+        return(
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onCompleteTodo={onCompleteTodo}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 
 export default TodoList;
