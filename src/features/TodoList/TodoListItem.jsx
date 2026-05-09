8d9151ddff22c6a3ac81d91b3633d2dbb0ffb73b
Tonny <tonnyan30@gmail.com>
2026-05-09 15:56:52 -0700

    Start of todoList editing

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
+
 function TodoListItem({todo = {}, onCompleteTodo}) {
     
+    const [isEditing, setIsEditing] = useState(false);
+
     return (
         <li>
-            <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => onCompleteTodo(todo.id)}
-            />
-            {todo.title}
+            <form>
+                {isEditing ? ( 
+                    <TextInputWithLabel 
+                        value={todo.title}
+                    /> ) : 
+                    (
+                        <label>
+                            <input
+                            type="checkbox"
+                            id={`checkbox${todo.id}`}
+                            checked={todo.isCompleted}
+                            onChange={() => onCompleteTodo(todo.id)}
+                             />
+                            <span onClick={() => setIsEditing(true)}>
+                                {todo.title} 
+                            </span>     
+                        </label>
+                    )
+                }
+            </form>
         </li>
     );
 }
