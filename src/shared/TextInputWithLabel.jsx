0e14dd92903557d38994a7bf42e4a5ec5b32b492
Tonny <tonnyan30@gmail.com>
2026-05-09 15:06:31 -0700

    Initial TextInputWithLabel file

---
+++ src/shared/TextInputWithLabel.jsx
@@ -0,0 +1,9 @@
+export default function TextInputWithLabel() {
+    
+    return (
+        <>
+            <label>Test</label>  
+            <input type="text"> </input>
+        </>      
+    );
+}
