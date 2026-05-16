239865af12a9ae0608edd7fb4db09e48ac422e12
Tonny <tonnyan30@gmail.com>
2026-05-16 14:59:16 -0700

    Copied todo stuff from App to todosPage

---
+++ src/features/Todos/TodosPage.jsx
@@ -0,0 +1,41 @@
+import TodoList from './features/Todos/TodoList/TodoList';
+import TodoForm from './features/Todos/TodoForm';
+import { useState } from 'react';
+
+export default function TodosPage() {
+    
+    const [todoList, setTodoList] = useState([]);
+
+    function updateTodo(editedTodo) {
+        const updatedTodos = todoList.map(todo => 
+        todo.id === editedTodo.id ? { ...editedTodo } : todo
+        );
+        setTodoList(updatedTodos);
+    }
+
+    function addTodo(todoTitle) {
+        const newTodo = {
+        id: Date.now(),
+        title: todoTitle,
+        isCompleted: false
+        };
+        setTodoList(previous => [newTodo, ...previous]);
+    }
+
+    function completeTodo (id) {  
+        const updatedTodoList = todoList.map((todo) => 
+        {
+        if (todo.id === id) return { ...todo, isCompleted: true}
+        else return todo;
+        })
+        setTodoList(updatedTodoList);
+    }
+
+    return (
+        <div>
+        <h1>My Todos</h1>
+        <TodoForm onAddTodo={addTodo}/>
+        <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
+        </div>
+    );
+}
