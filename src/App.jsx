a999ac128d2bfd453d5090e48da4754812bbec4a
Tonny <tonnyan30@gmail.com>
2026-05-16 16:02:54 -0700

    Part 4 Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import Header from './shared/Header'
 import TodosPage from './features/Todos/TodosPage'
 import { Logon } from './features/Logon'
+import { useState } from 'react'
 
 export default function App() {
+
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
 
   return(
     <>
       <Header />
-      <TodosPage />
-      <Logon />
+      {token ? (
+        <TodosPage token={token}/>
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken}/>
+      )}
     </>
   )
 }
