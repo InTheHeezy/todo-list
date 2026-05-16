44ff14fce014279e3567c8ade87ffe577bf5dd98
Tonny <tonnyan30@gmail.com>
2026-05-16 16:33:24 -0700

    Data Fetch in TodosPage

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,10 +1,35 @@
 import TodoList from './TodoList/TodoList';
 import TodoForm from './TodoForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function TodosPage() {
+export default function TodosPage({ token }) {
     
     const [todoList, setTodoList] = useState([]);
+    const [error, setError] = useState('');
+    const [isTodoListLoading, setIsTodoListLoading] = useState(false);
+
+    useEffect(() => {
+        if (!token) return;
+
+        const fetchTodos = async () => {
+            try {
+                setIsTodoListLoading(true);
+                const response = await fetch('/api/tasks', {
+                    method: 'GET',
+                    headers: { 'X-CSRF-TOKEN' : token },
+                    credentials: 'include'
+                });
+                if(response.status === 401) throw new Error('unauthorized');
+                if(!response.ok) throw new Error('Something went wrong');
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsTodoListLoading(false);
+            }
+        };
+
+        fetchTodos();
+    },[token]);
 
     function updateTodo(editedTodo) {
         const updatedTodos = todoList.map(todo => 
