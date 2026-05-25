e6a419ec5a0cdbe5c4e4c73102e680340cda0b1c
Tonny <tonnyan30@gmail.com>
2026-05-25 15:18:18 -0700

    sortBy file

---
+++ src/shared/SortBy.jsx
@@ -0,0 +1,23 @@
+export default function SortBy({
+    sortBy,
+    sortDirection,
+    onSortByChange,
+    onSortDirectionChange
+}) {
+    return (
+        <div>
+            <label htmlFor="sortBySelect">Sort By:</label>
+            <select id="sortBySelect" value={sortBy} onChange={onSortByChange}>
+                <option value="" disabled>Choose an Option</option>
+                <option value="Creation Date">Creation Date</option>
+                <option value="title">Title</option>
+            </select>
+            <label htmlFor="sortByDirection">Sort By:</label>
+            <select id="sortByDirection" value={sortDirection} onChange={onSortDirectionChange}>
+                <option value="" disabled>Choose an Option</option>
+                <option value='desc'>Descending</option>
+                <option value='asc'>Ascending</option>
+            </select>
+        </div>
+    )
+}
