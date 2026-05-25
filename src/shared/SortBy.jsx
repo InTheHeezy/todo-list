493896cc31cf55250025754538fb1ee3bc91208c
Tonny <tonnyan30@gmail.com>
2026-05-25 15:26:49 -0700

    onChange to target event value

--- src/shared/SortBy.jsx
+++ src/shared/SortBy.jsx
@@ -7,13 +7,21 @@ export default function SortBy({
     return (
         <div>
             <label htmlFor="sortBySelect">Sort By:</label>
-            <select id="sortBySelect" value={sortBy} onChange={onSortByChange}>
+            <select 
+                id="sortBySelect" 
+                value={sortBy} 
+                onChange={(e) => onSortByChange(e.target.value)}
+            >
                 <option value="" disabled>Choose an Option</option>
                 <option value="Creation Date">Creation Date</option>
                 <option value="title">Title</option>
             </select>
             <label htmlFor="sortByDirection">Sort By:</label>
-            <select id="sortByDirection" value={sortDirection} onChange={onSortDirectionChange}>
+            <select 
+                id="sortByDirection" 
+                value={sortDirection} 
+                onChange={(e) => onSortByChange(e.target.value)}
+            >
                 <option value="" disabled>Choose an Option</option>
                 <option value='desc'>Descending</option>
                 <option value='asc'>Ascending</option>
