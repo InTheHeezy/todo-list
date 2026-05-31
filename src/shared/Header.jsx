d8ceb46f92662886d8f50b7d2d6c325f3c847f92
Tonny <tonnyan30@gmail.com>
2026-05-31 16:19:07 -0700

    Header useAuth update

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,9 @@
-export default function Header({ onSetEmail, token, onSetToken }) {
+import { useAuth } from "../contexts/AuthContext"
+
+export default function Header() {
+
+    const { isAuthenticated } = useAuth();
+    
     return (
         <h1>Todo List</h1>
     )
