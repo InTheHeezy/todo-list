5418fb17f9b69b0bd83babf6b09950788ce07255
Tonny <tonnyan30@gmail.com>
2026-05-31 15:25:50 -0700

    login & logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -17,6 +17,76 @@ export function AuthContext({ children }){
 
     //Functions go here...
 
+    const login = async (userEmail, password) => {
+        
+        try {
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({email: userEmail, password }),
+                credentials: 'include',
+            };
+        
+            const res = await fetch('/api/users/logon', options);
+            const data = await res.json();
+
+            if (res.status === 200 && data.name && data.csrfToken) {
+            //Success: update state
+            setEmail(data.name);
+            setToken(data.csrfToken);
+            return { success: true };
+            } else {
+                return {
+                    success: false,
+                    error: `Authentication failed: ${data?.message}`
+                };
+            }
+        } catch (error) {
+            return {
+                success: false,
+                error: 'Network error during login'
+            };
+        }   
+    }
+
+    const logout = async () => {
+        
+        if(!token) {
+            setEmail('');
+            setToken('');
+            return { success: true };
+        }
+
+        try {
+            const options = {
+                method: 'POST',
+                headers: { 
+                    'Content-Type' : 'application/json',
+                    'X-CSRF-TOKEN' : token
+                },
+                credentials: 'include'
+            }
+            const response = await fetch('/users/logoff', options);
+
+            if (res.status === 200) {
+                return { success: true };
+            } else {
+                return {
+                    success: false,
+                    error: `Logout failed: ${data?.message}`
+                };
+            }
+        } catch (error) {
+            return {
+                success: false,
+                error: 'Network error during logout'
+            };
+        } finally {
+            setEmail('');
+            setToken('');
+        }
+    }
+
     const value = {
         email,
         token,
