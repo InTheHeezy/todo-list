00b8a260f55274f7b8a03f4656f4b0fc5bcbdab8
Tonny <tonnyan30@gmail.com>
2026-06-06 16:43:49 -0700

    LoginPage

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
+function LoginPage() {
+    const { login, isAuthenticated } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggingOn, setIsLoggingOn] = useState(false);
+    const [authError, setAuthError] = useState('');
+
+    const from = location.state?.from?.pathname || '/todos';
+
+    useEffect(() => {
+        if(isAuthenticated) {
+            navigate(from, { replace: true });
+        }
+    }, [isAuthenticated, navigate, from]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setAuthError('');
+        setIsLoggingOn(true)
+        
+        const result = await login(email, password);
+
+        if (!result.success) {
+            setAuthError(result.error);
+            setIsLoggingOn(false);
+        } 
+    }
+    
+    return (
+        <div>
+            {authError && <div>{authError}</div>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        id='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        disabled={isLoggingOn}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        id='password'
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        disabled={isLoggingOn}
+                    />
+                </div>
+                <button type="submit" disabled={isLoggingOn}>
+                    {isLoggingOn ? 'Logging on...' : 'Logon'}
+                </button>
+            </form>
+        </div>
+    );
+}
