fc7ab8e23f8e6fcdd9c9c8461eee7cf57dcd966f
Tonny <tonnyan30@gmail.com>
2026-06-07 14:29:52 -0700

    Navigation

---
+++ src/shared/Navigation.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+
+function navLinkStyle ({ isActive }) {
+    return {
+        fontWeight: isActive ? "bold" : "normal",
+        textStyle: isActive ? "underline" : "none"
+    };
+}
+
+function Navigation(){
+
+    const { isAuthenticated } = useAuth();
+
+    const listStyle = {
+        listStyle: 'none',
+        display: 'flex',
+        gap: '1rem',
+        padding: 0
+    };
+
+    return (
+        <nav>
+            <ul style={listStyle}>
+                <li>
+                    <NavLink to="/about" style={navLinkStyle}>About</NavLink>
+                </li>
+                {isAuthenticated ? (
+                    <NavLink to="/todos" style={navLinkStyle}>Todos</NavLink>
+                ) : (
+                    <NavLink to="/login" style={navLinkStyle}>Login</NavLink>
+                )}
+            </ul>
+        </nav>
+    )
+}
